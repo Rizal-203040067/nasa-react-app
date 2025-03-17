435865ec834dd8f6e24d12f373059bca5819fd64
Rizal Baihaqi <rizal.baihaqi007@gmail.com>
2025-03-17 16:45:18 +0700

    try to fix error on deploy

--- src/App.jsx
+++ src/App.jsx
@@ -15,8 +15,7 @@ function App() {
   useEffect(() => {
     async function fetchAPIData() {
       const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
-      const url =
-        "https://api.nasa.gov/planetary/apod" + "?api_key=${NASA_KEY}";
+      const url = "https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}";
 
       const today = new Date().toDateString();
       const localKey = "NASA-${today}";
