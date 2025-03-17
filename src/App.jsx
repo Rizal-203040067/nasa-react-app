2077c3afe8684bd1f7d6d8753c158ce882f72ba0
Rizal Baihaqi <rizal.baihaqi007@gmail.com>
2025-03-17 16:54:57 +0700

    fxing back to default

--- src/App.jsx
+++ src/App.jsx
@@ -14,8 +14,9 @@ function App() {
 
   useEffect(() => {
     async function fetchAPIData() {
-      // const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
-      const url = "https://api.nasa.gov/planetary/apod?api_key=${NASA_KEY}";
+      const NASA_KEY = import.meta.env.VITE_NASA_API_KEY;
+      const url =
+        "https://api.nasa.gov/planetary/apod" + "?api_key=${NASA_KEY}";
 
       const today = new Date().toDateString();
       const localKey = "NASA-${today}";
