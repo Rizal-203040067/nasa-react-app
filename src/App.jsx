112e8b3076b884f8483835f12ca13ff823104567
Rizal Baihaqi <rizal.baihaqi007@gmail.com>
2025-03-17 16:27:40 +0700

    fixing error cause by naming file

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import { useEffect, useState } from "react";
-import Footer from "./components/footer";
-import Main from "./components/main";
+import Footer from "./components/Footer";
+import Main from "./components/Main";
 import Sidebar from "./components/SideBar";
 
 function App() {
   const [data, setData] = useState(null);
-  const [showModal, setShowModal] = useState(false);
   const [loading, setLoading] = useState(false);
+  const [showModal, setShowModal] = useState(false);
 
   function handleToggleModal() {
     setShowModal(!showModal);
